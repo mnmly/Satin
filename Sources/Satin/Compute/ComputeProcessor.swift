f9708783a13f4d04b66f6f6da12280c3537b11a5
Hiroaki Yamane <i.am@mnmly.com>
2026-01-06 16:50:10 +0900

    make some properties *open*

--- Sources/Satin/Compute/ComputeProcessor.swift
+++ Sources/Satin/Compute/ComputeProcessor.swift
@@ -84,8 +84,8 @@ open class ComputeProcessor: ComputeShaderDelegate {
     }
 
     var _reset = true
-    var _index = 0
-    var _count = 0
+    open var _index = 0
+    open var _count = 0
     var _useDispatchThreads = false
 
     var parametersSubscription: AnyCancellable?
